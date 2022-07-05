5ec130aaa5127beb4d15dcde9860ba152019bf02
ChKim <chkim@ChKims-MacBook-Pro.local>
2022-07-05 11:45:00 +0100

    read beats from mp3 file using librosa

--- readMusic.py
+++ readMusic.py
@@ -1,6 +1,16 @@
 """
-Recording sound and analyze it 
+Extracting features using librosa 
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
+import librosa
+
+
+#filename = librosa.example('tropicana.mp3')
+
+y, sr = librosa.load('tropicana.mp3')
+
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
